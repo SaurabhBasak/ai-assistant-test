1dcc8c51afe2b23d7283d3dcd8e52515f01a8ce1
Saurabh Basak <saurabhbasak1@gmail.com>
2024-10-05 21:26:48 -0400

    Init

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,75 @@
+'use client'
+
+import Messages from "@/components/Messages";
+import Recorder, { mimeType } from "@/components/Recorder";
+import { SettingsIcon } from "lucide-react";
+
 import Image from "next/image";
+import { useRef } from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    const fileRef = useRef<HTMLInputElement | null>(null);
+    const submitButtonRef = useRef<HTMLButtonElement | null>(null);
+
+    const uploadAudio = (blob: Blob) => {
+        const url = URL.createObjectURL(blob);
+        
+        const file = new File([blob], 'audio.webm', { type: mimeType});
+
+        // set the file as the value of the hidden file input field
+        if (fileRef.current) {
+            const dataTransfer = new DataTransfer();
+            dataTransfer.items.add(file);
+            fileRef.current.files = dataTransfer.files;
+
+            // simulate a click & submit the form
+            if (submitButtonRef.current) {
+                submitButtonRef.current.click();
+            }
+        }
+    }
+
+    return (
+        <div className="bg-black h-screen overflow-y-auto">
+            {/* Header */}
+            <header className="flex justify-between fixed top-0 text-white w-full p-5">
+                <Image
+                    alt="Logo"
+                    src="https://i.imgur.com/fWHjATY.png"
+                    width={50}
+                    height={50}
+                    className="object-contain"
+                />
+
+                <SettingsIcon
+                    size={40}
+                    className="p-2 m-2 rounded-full cursor-pointer bg-purple-600 text-black transition-all ease-in-out duration-150 hover:bg-purple-700 hover:text-white"
+                />
+            </header>
+
+            {/* Form */}
+
+            <form className="flex flex-col bg-black">
+                <div className="flex-1 bg-gradient-to-b from-purple-500 to-black">
+                    <Messages />
+                </div>
+
+                {/* Hidden fields */}
+                <input type="file" name="audio" hidden ref={fileRef}/>
+                <button type="submit" name="submit" hidden ref={submitButtonRef}>
+                    Submit Audio
+                </button>
+
+                <div className="fixed bottom-0 w-full overflow-hidden bg-black rounded-t-3xl">
+                    {/* Recorder */}
+                    <Recorder uploadAudio={uploadAudio}/>
+
+                    <div>
+                        {/* Voice Synthesizer - output of the Assistant voice */}
+                    </div>
+                </div>
+            </form>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    );
 }
---
+++ components/Recorder.tsx
@@ -0,0 +1,106 @@
+"use client";
+
+import React, { useEffect, useRef, useState } from "react";
+import activeAssitantIcon from "@/img/active.gif";
+import notActiveAssistantIcon from "@/img/notactive.png";
+import Image from "next/image";
+import { useFormStatus } from "react-dom";
+
+export const mimeType = "audio/webm";
+
+function Recorder({ uploadAudio }: { uploadAudio: (blob: Blob) => void }) {
+    const mediaRecorder = useRef<MediaRecorder | null>(null);
+    const { pending } = useFormStatus();
+    const [permission, setPermission] = useState(false);
+    const [stream, setStream] = useState<MediaStream | null>(null);
+    const [recordingStatus, setRecordingStatus] = useState("inactive");
+    const [audioChunks, setAudioChunks] = useState<Blob[]>([]);
+
+    useEffect(() => {
+        getMicrophonePermission();
+    }, []);
+
+    const getMicrophonePermission = async () => {
+        if ("MediaRecorder" in window) {
+            try {
+                const streamData = await navigator.mediaDevices.getUserMedia({
+                    audio: true,
+                    video: false,
+                });
+                setPermission(true);
+                setStream(streamData);
+            } catch (err) {
+                if (err instanceof DOMException) {
+                    console.error("DOMException:", err.message);
+                } else if (err instanceof Error) {
+                    console.error("Error:", err.message);
+                    alert("Error: " + err.message);
+                } else {
+                    console.error("Unknown error:", err);
+                    alert("Unknown error: " + err);
+                }
+            }
+        } else {
+            alert("Mediarecorder not supported in browser.");
+        }
+    };
+
+    const startRecording = async () => {
+        if (stream === null || pending) return;
+
+        setRecordingStatus("recording");
+
+        // Create new media recorder instance using the stream
+        const media = new MediaRecorder(stream, { mimeType });
+        mediaRecorder.current = media;
+        mediaRecorder.current.start();
+
+        const localAudioChunks: Blob[] = [];
+
+        mediaRecorder.current.ondataavailable = (event) => {
+            if (typeof event.data === "undefined" || event.data.size === 0)
+                return;
+
+            localAudioChunks.push(event.data);
+        };
+
+        setAudioChunks(localAudioChunks);
+    };
+
+    return (
+        <div className="flex items-center justify-center text-white">
+            {!permission && (
+                <button onClick={getMicrophonePermission}>
+                    Get Microphone
+                </button>
+            )}
+
+            {pending && (
+                <Image
+                    src={activeAssitantIcon}
+                    alt="Recording"
+                    unoptimized
+                    priority={true}
+                    width={350}
+                    height={350}
+                    className="assistant grayscale"
+                />
+            )}
+
+            {permission && recordingStatus === "inactive" && !pending && (
+                <Image
+                src={notActiveAssistantIcon}
+                onClick={startRecording}
+                alt="Not Recording"
+                unoptimized
+                priority={true}
+                width={350}
+                height={350}
+                className="assistant cursor-pointer hover:scale-110 duration-150 transition-all ease-in-out"
+            />
+            )}
+        </div>
+    );
+}
+
+export default Recorder;
